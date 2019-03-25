4352217b51c7ae7ae2bcc5d0aacd62807035c545
Frederic DUARTE <geforst@gmail.com>
2019-03-25 12:23:21 +0100

    ex1_start

---
+++ first-script.js
@@ -0,0 +1,6 @@
+"use strict";
+var maVariable = 'Module formation Angular';
+var age = 40;
+age = '40';
+console.log("Age: " + age);
+//# sourceMappingURL=first-script.js.map
---
+++ first-script.ts
@@ -0,0 +1,5 @@
+let maVariable: string = 'Module formation Angular';
+
+let age:number|string = 40;
+age = '40'
+console.log(`Age: ${age}`);
